5ef100d513491f7ae98018ac3ba35fc93c37bdae
Niko Albertson <Albertsonniko@gmail.com>
2025-05-04 00:09:04 -0500

    Table fix, removed table script

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-# Script to force table creation on Render
-from db_orm import engine
-from models import Base
-
-if __name__ == "__main__":
-    print("Creating all tables...")
-    Base.metadata.create_all(engine)
-    print("Done.")
